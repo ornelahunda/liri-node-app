b0fc0e9f72eadd0a7a56b6d2a8bdab4ea94f0ff5
ornelahunda <ornelahunda@gmail.com>
2018-04-17 11:43:45 -0400

    making every part of liri commands functionable

--- README.md
+++ README.md
@@ -2,3 +2,50 @@
 
 #LIRI - Language Interpretation and Recognition Interface
 LIRI is like SIRI, but you run it  through command line node app, which  takes in arguments and outputs data.
+
+* Use LIRI to get your latest tweets, find out about a song, or a movie, or just choose a random action from your own random file.
+
+# Node Packages used:
+1.Twitter
+npm install twitter
+2.Spotify
+npm install spotify
+3.Request
+npm install request
+4.FS
+npm install fs
+
+#What Each Command Should Do
+
+* node liri.js my-tweets 
+
+This will show your last 20 tweets and when they were created at, in your terminal/bash window.
+And also all your entries will log in a text file, log.txt.
+
+* node liri.js spotify-this-song <song title here> 
+
+This will show the following information about the song in your terminal/bash window
+Artist(s)
+The song's name
+A preview link of the song from Spotify
+The album that the song is from
+if no song is provided then your program will default to
+"The Sign" by Ace of Base
+
+node liri.js movie-this <movie title here>
+
+This will output the following information to your terminal/bash window:
+Title of the movie.
+Year the movie came out.
+IMDB Rating of the movie.
+Country where the movie was produced.
+Language of the movie.
+Plot of the movie.
+Actors in the movie.
+Rotten Tomatoes Rating.
+
+* node liri.js do-what-it-says
+
+This uses the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+
--- liri.js
+++ liri.js
@@ -1,13 +1,10 @@
 // This is included to read and set any environment variables with the dotenv package
 var dotenv = require("dotenv").config();
-// console.log(dotenv);
 
-// Load the user keys
+// Passes Twitter and Spotify keys into call to their API.
 var keys = require('./keys/ keys.js');
-// console.log(keys);
 
 // Node modules
-
 // Requesting twitter package
 var Twitter = require('twitter');
 
@@ -18,7 +15,6 @@ var request = require('request');
 // Load the fs package to read and write
 var fs = require('fs');
 
-
 // READ COMMAND LINE ARGUMENTS
 var arg = process.argv;
 
@@ -28,7 +24,7 @@ var arg = process.argv;
 // The second will be the song or movie title, etc.
 var action = "";
 var searchFor = "";
-// check if we min 3 arg in argv array
+// check if we have 3 arg in argv array
 if (arg.length > 2) {
     action = process.argv[2];
     if (arg.length > 4) {
@@ -53,9 +49,9 @@ switch (action) {
         getTweets();
         break;
 
-        // case "spotify-this-song":
-        //     spotifySong();
-        //     break;
+    case "spotify-this-song":
+        spotifySong();
+        break;
 
     case "movie-this":
         movie();
@@ -69,9 +65,6 @@ switch (action) {
 
 
 // TWITTER 
-// Passes Twitter keys into call to Twitter API.
-
-
 
 // This function will  be used to get back latest 20 tweets with a request from command line
 function getTweets() {
@@ -82,7 +75,7 @@ function getTweets() {
     };
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error && response.statusCode == 200) {
-            fs.appendFile('log.txt', ('=============== Tweets Entry Start ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n'), function (err) {
+            fs.appendFile('log.txt', (' ================ LOG ENTRY START ================\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv + '\r\n \r\nDATA OUTPUT:\r\n'), function (err) {
                 if (err) throw err;
             });
             console.log(' ');
@@ -97,112 +90,71 @@ function getTweets() {
                     if (err) throw err;
                 });
             }
-            fs.appendFile('log.txt', ('=============== LOG ENTRY END ===============\r\n \r\n'), function (err) {
+            fs.appendFile('log.txt', ('================ LOG ENTRY END ================\r\n \r\n'), function (err) {
                 if (err) throw err;
             });
         }
     });
 }
 
-
-// function spotifySong() {
-
-//     var spotify = new Spotify(keys.spotify);
-
-//     var song;
-//     if (song === '') {
-//         search = 'The Sign Ace Of Base';
-//     } else {
-//         search = song;
-//     }
-
-// }
-// // Append the command to the log file
-// fs.appendFile('./log.txt', 'User Command: node liri.js spotify-this-song ' + song + '\n\n', function (err) {
-//     if (err) throw err;
-// });
-
-// If no song is provided, LIRI defaults to 'The Sign' by Ace Of Base
+// spotify calling function
 
 
-// spotify
-//     .request('https://api.spotify.com/v1/search?q=' + searchFor + '&type=track')
-//     .catch(function (err) {
-//         console.error('Error occurred: ' + err);
-//     });
-//     .then(function (data) {
-//         if (!error && response.statusCode == 200) {
-//             jsonBody = JSON.parse(body);
-//             console.log(data);
-//         }
-//     });
+function spotifySong() {
 
-// }
+    var spotify = new Spotify(keys.spotify);
 
-//         spotify
-//         .request('https://api.spotify.com/v1/search?q=' + searchFor + '&type=track', function(error, response, body) {
-//                     if (!error && response.statusCode == 200) {
-//                         jsonBody = JSON.parse(body);
-//           console.log(data); 
-//                     } .catch(function(err) {
-//           console.error('Error occurred: ' + err); 
-//         });
-// }
-//     }
-//     // Default search value if no song is given
-//     if (searchFor == "") {
-//         searchFor = "The Sign Ace of Base";
-//     } else{
-//         request('https://api.spotify.com/v1/search?q=' + searchFor + '&type=track', function (error, response, body) {
-//             .then(function () {
+    // Default search value if no song is given
+    if (searchFor === undefined) {
+        searchFor = "The Sign Ace of Base";
+        //    console.log(searchFor); 
+    }
+    var url = 'https://api.spotify.com/v1/search?q=' + searchFor + '&type=track';
+    spotify
+        .request(url)
+        .then(function (data) {
+            // console.log(data); 
+            console.log(' ');
+            var artist = data.tracks.items[0].artists[0].name;
+            console.log('Artist:' + artist);
+            var song = data.tracks.items[0].name;
+            console.log('Song title: ' + song);
+            var preview = data.tracks.items[0].preview_url;
+            console.log('Preview Link: ' + preview);
+            var album = data.tracks.items[0].album.name;
+            console.log('Album: ' + album);
+            console.log(' ');
 
-//                     if (!error && response.statusCode == 200) {
-//                         jsonBody = JSON.parse(body);
-//                         console.log(' ');
-//                         console.log('Artist: ' + jsonBody.tracks.items[0].artists[0].name);
-//                         console.log('Song: ' + jsonBody.tracks.items[0].name);
-//                         console.log('Preview Link: ' + jsonBody.tracks.items[0].preview_url);
-//                         console.log('Album: ' + jsonBody.tracks.items[0].album.name);
-//                         console.log(' ');
-//                         fs.appendFile('terminal.log', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv +
-//                             '\r\n \r\nDATA OUTPUT:\r\n' + 'Artist: ' + jsonBody.tracks.items[0].artists[0].name +
-//                             '\r\nSong: ' + jsonBody.tracks.items[0].name + '\r\nPreview Link: ' + jsonBody.tracks.items[0].preview_url + '\r\nAlbum: ' + jsonBody.tracks.items[0].album.name + '\r\n=============== LOG ENTRY END ===============\r\n \r\n'), function (err) {
-//                             if (err) throw err;
-//                         });
-//                     })
-//             }
-//         });
+            fs.appendFile('log.txt', ('=============== LOG ENTRY START ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS:\r\n$: ' + process.argv +
+                '\r\n \r\nDATA OUTPUT:\r\n' + 'Artist: ' + artist + '\r\nSong: ' + song + '\r\nPreview Link: ' + preview + '\r\nAlbum: ' + album +
+                '\r\n=============== LOG ENTRY END ===============\r\n \r\n'), function (err) {
+                if (err) throw err;
+            });
 
-// }
-// }}
+        })
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
+
+
+}
 
 
 
 // Uses fs node package to take the text inside random.txt,
-// and do something with it.
+// and return a response from spotify.
 function random() {
-
-    fs.readFile("random.txt", "utf8", function (err, data) {
-        if (err) {
-            logOutput.error(err);
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
+            console.log(error);
         } else {
-
-            // Creates array with data.
-            var randomArray = data.split(",");
-
-            // Sets action to first item in array.
-            action = randomArray[0];
-
-            // Sets optional third argument to second item in array.
-            argument = randomArray[1];
-
-            // Calls main controller to do something based on action and argument.
-            doSomething(action, argument);
-        }
+            var commandsArr = data.split(',');
+            if (commandsArr[0] === 'spotify-this-song') {
+                spotifySong(commandsArr[1]);
+            } 
+        } 
     });
 }
-
-
 // Function movie, is going to be used to give user information about the movie they want to search
 // This function should give back:
 // * Title of the movie.
@@ -213,30 +165,28 @@ function random() {
 // * Language of the movie.
 // * Plot of the movie.
 // * Actors in the movie.
-
+// '&tomatoes=true&r=json' 
 function movie() {
     var queryUrl = "http://www.omdbapi.com/?t=" + searchFor + "&y=&plot=short&apikey=trilogy";
-    console.log(queryUrl);
+    // console.log(queryUrl);
     request(queryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            // console.log("Release Year: " + JSON.parse(body).Year);
+          
             jsonBody = JSON.parse(body);
             console.log(' ');
             console.log('Title: ' + jsonBody.Title);
             console.log('Year: ' + jsonBody.Year);
             console.log('IMDb Rating: ' + jsonBody.imdbRating);
-            console.log('Rotten Tomatoes Rating: ' + jsonBody.tomatoRating);
+            console.log('Rotten Tomatoes Rating: ' + jsonBody.Ratings[1].Value);
             console.log('Country: ' + jsonBody.Country);
             console.log('Language: ' + jsonBody.Language);
             console.log('Plot: ' + jsonBody.Plot);
             console.log('Actors: ' + jsonBody.Actors);
 
 
-            console.log('Rotten Tomatoes URL: ' + jsonBody.tomatoURL);
-
-            //             fs.appendFile('log.txt', ('=============== LOG ENTRY BEGIN ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS: ' + process.argv + '\r\nDATA OUTPUT:\r\n' + 'Title: ' + jsonBody.Title + '\r\nYear: ' + jsonBody.Year + '\r\nIMDb Rating: ' + jsonBody.imdbRating + '\r\nCountry: ' + jsonBody.Country + '\r\nLanguage: ' + jsonBody.Language + '\r\nPlot: ' + jsonBody.Plot + '\r\nActors: ' + jsonBody.Actors + '\r\nRotten Tomatoes Rating: ' + jsonBody.tomatoRating + '\r\nRotten Tomatoes URL: ' + jsonBody.tomatoURL + '\r\n =============== LOG ENTRY END ===============\r\n \r\n'), function (err) {
-            //                 if (err) throw err;
-            //             });
+            fs.appendFile('log.txt', ('=============== LOG ENTRY START ===============\r\n' + Date() + '\r\n \r\nTERMINAL COMMANDS: ' + process.argv + '\r\nDATA OUTPUT:\r\n' + 'Title: ' + jsonBody.Title + '\r\nYear: ' + jsonBody.Year + '\r\nIMDb Rating: ' + jsonBody.imdbRating + '\r\nCountry: ' + jsonBody.Country + '\r\nLanguage: ' + jsonBody.Language + '\r\nPlot: ' + jsonBody.Plot + '\r\nActors: ' + jsonBody.Actors + '\r\nRotten Tomatoes Rating: ' + jsonBody.Ratings[1].Value + '\r\n =============== LOG ENTRY END ===============\r\n \r\n'), function (err) {
+                if (err) throw err;
+            });
         }
     });
 }
